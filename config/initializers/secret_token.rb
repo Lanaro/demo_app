5e2201b3884959a3d8319393e72de61c4faa3a13
Raymond Chan <raymond.chan@sendgrid.com>
2013-07-01 22:48:01 +0000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '3fdb0bd7b605d23df12935b39fe0b486fb83c7408d2f899561aa0433f7261bd61aa6006bc5062dfe4a59119574fa10a9e744db3cf0d652cecc6371e5649eb503'
